5174edcfbec38a938f8af5f29d3dc75296b03cca
smartwalle <smartwalle@gmail.com>
2019-04-10 14:35:39 +0800

    update default plugin

--- plugin.go
+++ plugin.go
@@ -4,6 +4,8 @@ import (
 	_ "github.com/micro/go-plugins/client/grpc"
 	_ "github.com/micro/go-plugins/server/grpc"
 
+	_ "github.com/micro/go-plugins/registry/etcdv3"
+
 	_ "github.com/smartwalle/pks/plugins/client/pks_grpc"
 	_ "github.com/smartwalle/pks/plugins/server/pks_grpc"
 
